b9542e6fc7905d729b67104e6a86d472959680bc
natlose <natlose@gmail.com>
2019-05-18 00:43:21 +0200

    DbContext

---
+++ RML_UserControl/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RML_UserControl
+{
+    public enum AddressKind { RES, MAIL, DEL }
+
+    public class Address
+    {
+        public int Id { get; set; }        
+        public string Kind { get; set; } // nincs enum támogatás az EF6-ban, a megjelenítési réteg fog konvertálni
+        public string Country3166 { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+
+        public int FI_Person { get; set; }
+        public Person Person { get; set; }
+    }
+}
---
+++ RML_UserControl/MyData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace RML_UserControl
+{
+    class MyData : DbContext
+    {
+        // VS-ben, a projekt tulajdonságlapjának Settings fülén felvettem a mydata sort.
+        // Ebből a VS generálta a Properties\Settings.Designer.cs forrást és az App.config xml fájlt
+        // A Settings osztály felolvassa a configot, már csak használni kell
+        public MyData() : base(Properties.Settings.Default.mydata) { }
+        // Vedd észre, hogy az EF6-ban nincs OnConfiguring! Itt a konstruktorban kell megoldani a
+        // connectionstring átadását.
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Hiányzik egy-két dolog a modelBuilder-ben is
+
+            #region Person            
+            //modelBuilder.Entity<Person>().Property(p => p.Id)
+            //    .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Person>().Property(p => p.Titles)
+                .HasMaxLength(10)
+                .IsUnicode();
+            modelBuilder.Entity<Person>().Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode();
+            modelBuilder.Entity<Person>().Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode();
+            modelBuilder.Entity<Person>().Property(p => p.Phone)
+                .HasMaxLength(30);
+            modelBuilder.Entity<Person>().Property(p => p.Mobile)
+                .HasMaxLength(30);
+            modelBuilder.Entity<Person>().Property(p => p.Email)
+                .HasMaxLength(80);
+            // EF6-ban az egy-sok kapcsolatot a principal irányából konfigurálom
+            modelBuilder.Entity<Person>()
+                .HasMany<Address>(p => p.Addresses)
+                .WithRequired(a => a.Person)
+                .HasForeignKey(a => a.FI_Person);
+            modelBuilder.Entity<Person>().ToTable("Person");
+            #endregion
+
+            #region Address
+            modelBuilder.Entity<Address>().Property(b => b.Kind)
+                .IsRequired();
+                //.HasColumnType("char(10)");  Ez nem kell, mert nincs enum...
+            //.HasDefaultValue()
+            //.HasConversion(new EnumToStringConverter<AddressKind>());  Nincs enum támogatás, a konverziót a megjelenítési rétegben kell megoldani
+            modelBuilder.Entity<Address>().Property(a => a.Country3166)
+                .IsRequired()
+                .HasMaxLength(2);
+            modelBuilder.Entity<Address>().Property(a => a.Zip)
+                .IsRequired()
+                .HasMaxLength(10);
+            modelBuilder.Entity<Address>().Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(30);
+            modelBuilder.Entity<Address>().Property(a => a.Line1)
+                .IsRequired()
+                .HasMaxLength(30);
+            modelBuilder.Entity<Address>().Property(a => a.Line2)
+                .HasMaxLength(30);
+            //modelBuilder.Entity<Address>()   //Ezt a kapcsolatot a másik irányból konfiguráltuk
+            //    .HasOne(a => a.Person)
+            //    .WithMany(p => p.Addresses)
+            //    .HasForeignKey(a => a.FI_Person);
+
+            modelBuilder.Entity<Address>().ToTable("Address");
+            #endregion
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
